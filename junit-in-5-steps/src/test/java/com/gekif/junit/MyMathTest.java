97b8deae79bbc11ff1b46705a4118bdcb7604057
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-11-05 12:56:18 +0700

    03 Introduction to JUnit in 5 Steps: 042 Step 5  Important annotations

--- junit-in-5-steps/src/test/java/com/gekif/junit/MyMathTest.java
+++ junit-in-5-steps/src/test/java/com/gekif/junit/MyMathTest.java
@@ -1,24 +1,45 @@
 package com.gekif.junit;
 
-import org.junit.Test;
+import org.junit.*;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 
 public class MyMathTest {
+
+    MyMath myMath = new MyMath();
+
+    @Before
+    public void before() {
+        System.out.println("Before");
+    }
+
+    @After
+    public void after() {
+        System.out.println("After");
+    }
+
+    @BeforeClass
+    public static void beforeClass() {
+        System.out.println("Before Class");
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        System.out.println("After Class");
+    }
 
     // MyMath.sum
     // 1,2,3 => 6
 
     @Test
     public void sum_with3numbers() {
-        MyMath myMath = new MyMath();
+        System.out.println("Test1");
         assertEquals(6, myMath.sum(new int[]{1, 2, 3}));
     }
 
     @Test
     public void sum_with1numbers() {
-        MyMath myMath = new MyMath();
+        System.out.println("Test2");
         assertEquals(3, myMath.sum(new int[]{3}));
     }
 
