3469f698f97ff0dbd1cd72dc64e2427c29bcb80e
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-11-05 11:13:51 +0700

    03 Introduction to JUnit in 5 Steps: 040 Step 3  First Code and First Unit Test

--- junit-in-5-steps/src/test/java/com/gekif/junit/MyMathTest.java
+++ junit-in-5-steps/src/test/java/com/gekif/junit/MyMathTest.java
@@ -2,15 +2,24 @@ package com.gekif.junit;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 public class MyMathTest {
 
+    // MyMath.sum
+    // 1,2,3 => 6
+
     @Test
-    public void test() {
+    public void sum_with3numbers() {
+        MyMath myMath = new MyMath();
+        assertEquals(6, myMath.sum(new int[]{1, 2, 3}));
+    }
 
-        // Absence of failure is success
-
+    @Test
+    public void sum_with1numbers() {
+        MyMath myMath = new MyMath();
+        assertEquals(3, myMath.sum(new int[]{3}));
     }
 
 }
