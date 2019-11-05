dc0325dbfaac9babf25477e409f796158782c254
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-11-05 11:04:36 +0700

    03 Introduction to JUnit in 5 Steps: 039 Step 2  First JUnit Project and Green Bar

---
+++ junit-in-5-steps/src/main/java/com/gekif/junit/MyMath.java
@@ -0,0 +1,13 @@
+package com.gekif.junit;
+
+public class MyMath {
+    int sum(int[] numbers) {
+        int sum = 0;
+
+        for (int i : numbers) {
+            sum += i;
+        }
+
+        return sum;
+    }
+}
---
+++ junit-in-5-steps/src/test/java/com/gekif/junit/MyMathTest.java
@@ -0,0 +1,16 @@
+package com.gekif.junit;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class MyMathTest {
+
+    @Test
+    public void test() {
+
+        // Absence of failure is success
+
+    }
+
+}
