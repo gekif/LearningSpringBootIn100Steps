e0fa3cf6d328b68b5d44a29c06dd7feec10d15f3
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-11-07 10:57:41 +0700

    02 Web Application with Spring Boot: 017 Step 07 Your First HTML form

--- spring-boot-first-web-application/src/main/java/com/gekif/springboot/web/springbootfirstwebapplication/controller/LoginController.java
+++ spring-boot-first-web-application/src/main/java/com/gekif/springboot/web/springbootfirstwebapplication/controller/LoginController.java
@@ -3,18 +3,21 @@ package com.gekif.springboot.web.springbootfirstwebapplication.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
-    // Model
-
-
-    @RequestMapping("/login")
-    public String loginMessage(@RequestParam String name, ModelMap model) {
-        model.put("name", name);
+    @RequestMapping(value= "/login", method = RequestMethod.GET)
+    public String showLoginMessage(ModelMap model) {
         return "login";
+    }
+
+    @RequestMapping(value= "/login", method = RequestMethod.POST)
+    public String showWelcomeMessage(ModelMap model, @RequestParam String name) {
+        model.put("name", name);
+        return "welcome";
     }
 
 }
