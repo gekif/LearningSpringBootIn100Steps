c148e14a24451a90ac8e8259ecf82feb5c18d6ca
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-11-07 16:47:34 +0700

    02 Web Application with Spring Boot: 022 Step 12 Session vs Model vs Request - SessionAttributes

--- spring-boot-first-web-application/src/main/java/com/gekif/springboot/web/springbootfirstwebapplication/controller/ToDoController.java
+++ spring-boot-first-web-application/src/main/java/com/gekif/springboot/web/springbootfirstwebapplication/controller/ToDoController.java
@@ -8,8 +8,10 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
+@SessionAttributes("name")
 public class ToDoController {
 
     @Autowired
@@ -18,7 +20,8 @@ public class ToDoController {
 
     @RequestMapping(value= "/list-todos", method = RequestMethod.GET)
     public String showTodos(ModelMap model) {
-        model.put("todos", service.retrieveTodos("gekif"));
+        String name = (String) model.get("name");
+        model.put("todos", service.retrieveTodos(name));
         return "list-todos";
     }
 
--- spring-boot-first-web-application/src/main/webapp/WEB-INF/jsp/list-todos.jsp
+++ spring-boot-first-web-application/src/main/webapp/WEB-INF/jsp/list-todos.jsp
@@ -5,5 +5,7 @@
 <body>
     Here are the list of your todos:
     ${todos}
+    <br>
+    Your Name is : ${name}
 </body>
 </html>
