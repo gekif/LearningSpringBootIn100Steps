a7d3bb5fcd996a2d99c6a76b05143ec0cdb1263e
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-11-07 16:59:43 +0700

    02 Web Application with Spring Boot: 023 Step 13 Add new todo

--- spring-boot-first-web-application/src/main/java/com/gekif/springboot/web/springbootfirstwebapplication/controller/ToDoController.java
+++ spring-boot-first-web-application/src/main/java/com/gekif/springboot/web/springbootfirstwebapplication/controller/ToDoController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import java.util.Date;
+
 @Controller
 @SessionAttributes("name")
 public class ToDoController {
@@ -17,12 +19,22 @@ public class ToDoController {
     @Autowired
     TodoService service;
 
-
     @RequestMapping(value= "/list-todos", method = RequestMethod.GET)
     public String showTodos(ModelMap model) {
         String name = (String) model.get("name");
         model.put("todos", service.retrieveTodos(name));
         return "list-todos";
+    }
+
+    @RequestMapping(value= "/add-todo", method = RequestMethod.GET)
+    public String showAddTodoPage(ModelMap model) {
+        return "todo";
+    }
+
+    @RequestMapping(value = "/add-todo", method = RequestMethod.POST)
+    public String addTodo(ModelMap model, @RequestParam String desc) {
+        service.addTodo((String) model.get("name"), desc, new Date(), false);
+        return "redirect:/list-todos";
     }
 
 
--- spring-boot-first-web-application/src/main/webapp/WEB-INF/jsp/list-todos.jsp
+++ spring-boot-first-web-application/src/main/webapp/WEB-INF/jsp/list-todos.jsp
@@ -3,9 +3,9 @@
     <title>First Web Application</title>
 </head>
 <body>
-    Here are the list of your todos:
+    Here are the list of your ${names}'s todos:
     ${todos}
     <br>
-    Your Name is : ${name}
+    <a href="/add-todo">Add a Todo</a>
 </body>
 </html>
