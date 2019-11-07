59885b181570c18fe8fde5309cecc36b1fea7422
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-11-07 10:12:54 +0700

    02 Web Application with Spring Boot: 014 Step 04 Redirect to Login JSP - ResponseBody and View Resolver

--- spring-boot-first-web-application/src/main/java/com/gekif/springboot/web/springbootfirstwebapplication/controller/LoginController.java
+++ spring-boot-first-web-application/src/main/java/com/gekif/springboot/web/springbootfirstwebapplication/controller/LoginController.java
@@ -2,15 +2,13 @@ package com.gekif.springboot.web.springbootfirstwebapplication.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
 
     @RequestMapping("/login")
-    @ResponseBody
     public String loginMessage() {
-        return "Hello World Modified";
+        return "login";
     }
 
 }
