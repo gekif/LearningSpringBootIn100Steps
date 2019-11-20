0eace0840c8f1af407f72a9a234d34d08fe34246
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-11-20 13:58:43 +0700

    05 Spring Boot Deep Dive with a simple API: 052 Step 02 Creating your first RestController

---
+++ first-springboot-project/src/main/java/com/gekif/springboot/WelcomeController.java
@@ -0,0 +1,29 @@
+package com.gekif.springboot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class WelcomeController {
+
+    @Autowired
+    private WelcomeService service;
+
+    @RequestMapping("/welcome")
+    public String welcome() {
+        return service.retrieveWelcomeMessage();
+    }
+
+}
+
+
+@Component
+class WelcomeService {
+
+    public String retrieveWelcomeMessage() {
+        return "Good Morning";
+    }
+}
