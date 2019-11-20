022564861950fdd66a8956519343115ff22aa99e
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-11-20 15:31:06 +0700

    05 Spring Boot Deep Dive with a simple API: 062 Step 10 Create a Service to add a new question to survey PostMapping Postman

--- first-springboot-project/src/main/java/com/gekif/springboot/controller/SurveyController.java
+++ first-springboot-project/src/main/java/com/gekif/springboot/controller/SurveyController.java
@@ -4,10 +4,11 @@ package com.gekif.springboot.controller;
 import com.gekif.springboot.model.Question;
 import com.gekif.springboot.service.SurveyService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 
@@ -17,14 +18,29 @@ public class SurveyController {
     @Autowired
     private SurveyService surveyService;
 
+
     @GetMapping("/surveys/{surveyId}/questions")
     public List<Question> retrieveQuestionsForSurvey(@PathVariable String surveyId) {
         return surveyService.retrieveQuestions(surveyId);
     }
 
+
     @GetMapping("/surveys/{surveyId}/questions/{questionId}")
     public Question retrieveDetailsForQuestion(@PathVariable String surveyId, @PathVariable String questionId) {
         return surveyService.retrieveQuestion(surveyId, questionId);
+    }
+
+
+    @PostMapping("/surveys/{surveyId}/questions")
+    public ResponseEntity<Void> addQuestionToSurvey(@PathVariable String surveyId, @RequestBody Question newQuestion) {
+        Question question = surveyService.addQuestion(surveyId, newQuestion);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(question.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
+
     }
 
 }
