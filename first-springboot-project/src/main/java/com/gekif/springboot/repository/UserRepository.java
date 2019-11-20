917d54e2928a4b83144dee0aa4363d5c26dee72a
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-11-20 16:32:31 +0700

    05 Spring Boot Deep Dive with a simple API: 072 Step 19 In Memory Database H2 Console and add a new JPA Repository Method-en

--- first-springboot-project/src/main/java/com/gekif/springboot/repository/UserRepository.java
+++ first-springboot-project/src/main/java/com/gekif/springboot/repository/UserRepository.java
@@ -3,8 +3,8 @@ package com.gekif.springboot.repository;
 import com.gekif.springboot.jpa.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface UserRepository extends CrudRepository<User, Long> {
-
-
-
+    List<User> findByRole(String role);
 }
