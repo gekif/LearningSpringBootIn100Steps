482cb9c100067739041d6d9336819acc28da0019
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-11-20 17:31:02 +0700

    05 Spring Boot Deep Dive with a simple API: 075 Step 21 Spring Boot Integration Test Part 2-en

--- first-springboot-project/src/test/java/com/gekif/springboot/controller/SurveyControllerIT.java
+++ first-springboot-project/src/test/java/com/gekif/springboot/controller/SurveyControllerIT.java
@@ -3,20 +3,60 @@ package com.gekif.springboot.controller;
 import com.gekif.springboot.Application;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertTrue;
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SurveyControllerIT {
 
+
     @LocalServerPort
     private int port;
 
-    @Test
-    public void test() {
-        System.out.println("PORT PORT " + port);
+
+    /*@Test
+    public void testJsonAssert() {
+        String expect = "{id: 2, role: Admin}";
+        String actual = "{id: 2, name:Ranga, role: Admin}";
+
+        JSONAssert.assertEquals(expect, actual,  false);
     }
+*/
+
+    @Test
+    public void testRetrieveSurveyQuestion() {
+
+        String url = "http://localhost:" + port + "/surveys/Survey1/questions/Question1";
+
+        TestRestTemplate restTemplate = new TestRestTemplate();
+
+//        String output = restTemplate.getForObject(url, String.class);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+
+        HttpEntity entity = new HttpEntity<String>(null, headers);
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+
+        System.out.println("Response:  " + response.getBody());
+
+        assertTrue(response.getBody().contains("\"id\":\"Question1\""));
+        assertTrue(response.getBody().contains("\"description\":\"Largest Country in the World\""));
+
+        String expected = "{id:Question1,description:Largest Country in the World,correctAnswer:Russia}";
+        JSONAssert.assertEquals(expected, response.getBody(), false);
+
+    }
+
+
 }
