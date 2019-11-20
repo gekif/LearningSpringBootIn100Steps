4dcdfe4d0a9d1d33b5089d97dd418dc45adcfd9e
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-11-20 16:12:45 +0700

    05 Spring Boot Deep Dive with a simple API: 069 Step 17 Advanced Application Configuration - Type Safe Configuration Properties

--- first-springboot-project/src/main/java/com/gekif/springboot/controller/WelcomeController.java
+++ first-springboot-project/src/main/java/com/gekif/springboot/controller/WelcomeController.java
@@ -1,15 +1,22 @@
 package com.gekif.springboot.controller;
 
+import com.gekif.springboot.configuration.BasicConfiguration;
 import com.gekif.springboot.service.WelcomeService;
 import org.apache.commons.text.WordUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class WelcomeController {
 
     @Autowired
     private WelcomeService service;
+
+    @Autowired
+    private BasicConfiguration configuration;
 
     @GetMapping(value = "/welcome")
     public String welcome() {
@@ -25,6 +32,16 @@ public class WelcomeController {
     @GetMapping(value = "/welcome-message-property")
     public String welcomeFromMessageProperty() {
         return service.retrieveMessageFromPropertyFile();
+    }
+
+    @GetMapping(value = "/dynamic-configuration")
+    public Map dynamicConfiguration() {
+        Map map = new HashMap();
+        map.put("message", configuration.getMessage());
+        map.put("number", configuration.getNumber());
+        map.put("value", configuration.isValue());
+
+        return map;
     }
 
 }
