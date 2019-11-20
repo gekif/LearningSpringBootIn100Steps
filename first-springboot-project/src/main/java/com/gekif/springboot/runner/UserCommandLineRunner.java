6f36f74ee3521eed72989a1e0240ecc30d5a1ead
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-11-20 16:28:08 +0700

    05 Spring Boot Deep Dive with a simple API: 071 Step 18 Spring Boot Starter  Spring Data JPA with CommandLineRunner Part 2

--- first-springboot-project/src/main/java/com/gekif/springboot/runner/UserCommandLineRunner.java
+++ first-springboot-project/src/main/java/com/gekif/springboot/runner/UserCommandLineRunner.java
@@ -1,14 +1,32 @@
 package com.gekif.springboot.runner;
 
+import com.gekif.springboot.jpa.User;
+import com.gekif.springboot.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserCommandLineRunner implements CommandLineRunner {
 
+    private static final Logger log = LoggerFactory
+            .getLogger(UserCommandLineRunner.class);
+
+    @Autowired
+    UserRepository repository;
 
     @Override
     public void run(String... strings) throws Exception {
-        System.out.println("UserCommandLineRunner");
+        repository.save(new User("Ranga", "Admin"));
+        repository.save(new User("Ravi", "User"));
+        repository.save(new User("Satish", "Admin"));
+        repository.save(new User("Raghu", "User"));
+
+        for (User user : repository.findAll()) {
+            log.info(user.toString());
+        }
+
     }
 }
