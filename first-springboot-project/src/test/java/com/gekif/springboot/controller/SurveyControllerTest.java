9db5c8683eff198d67da856d963c6e48f67c1505
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-11-21 10:43:40 +0700

    05 Spring Boot Deep Dive with a simple API: 080 Step 25 Writing Unit test for createTodo

--- first-springboot-project/src/test/java/com/gekif/springboot/controller/SurveyControllerTest.java
+++ first-springboot-project/src/test/java/com/gekif/springboot/controller/SurveyControllerTest.java
@@ -9,7 +9,10 @@ import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -17,6 +20,8 @@ import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
 
 
 @RunWith(SpringRunner.class)
@@ -32,6 +37,7 @@ public class SurveyControllerTest {
 
     @Test
     public void retrieveDetailsForQuestion() throws Exception {
+
         Question mockQuestion = new Question("Question1",
                 "Largest Country in the World", "Russia", Arrays.asList(
                 "India", "Russia", "United States", "China"));
@@ -54,6 +60,38 @@ public class SurveyControllerTest {
                 expected,
                 result.getResponse().getContentAsString(),
                 false);
+
+    }
+
+
+    @Test
+    public void createSurveyQuestion() throws Exception {
+        Question mockQuestion = new Question("1", "Smallest Number", "1",
+                Arrays.asList("1", "2", "3", "4"));
+
+        String questionJson = "{\"description\":\"Smallest Number\",\"correctAnswer\":\"1\",\"options\":[\"1\",\"2\",\"3\",\"4\"]}";
+
+        // surveyService.addQuestion to respond back with mockQuestion
+        Mockito.when(
+                surveyService.addQuestion(Mockito.anyString(),
+                        Mockito.any(Question.class))).thenReturn(mockQuestion);
+
+        // Send question as body to /surveys/Survey1/questions
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/surveys/Survey1/questions")
+                .accept(MediaType.APPLICATION_JSON)
+                .content(questionJson)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        MvcResult result =  mockMvc.perform(requestBuilder).andReturn();
+        MockHttpServletResponse response = result.getResponse();
+
+        // Assert
+        assertEquals(HttpStatus.CREATED.value(), response.getStatus());
+        assertEquals("http://localhost/surveys/Survey1/questions/1", response.getHeader(HttpHeaders.LOCATION));
+
+
+
 
     }
 
