561551beedbd9040275715dc243c56235f6672a2
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-11-21 13:45:03 +0700

    06 Introduction to JPA With Spring Boot in 10 Steps: 095 Step 10  More JPA Repository  findById and findAll

--- jpa-in-10-steps/src/main/java/com/gekif/learning/jpa/jpain10steps/UserRepositoryCommandLineRunner.java
+++ jpa-in-10-steps/src/main/java/com/gekif/learning/jpa/jpain10steps/UserRepositoryCommandLineRunner.java
@@ -9,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Optional;
+
 @Component
 public class UserRepositoryCommandLineRunner implements CommandLineRunner {
 
@@ -22,7 +25,13 @@ public class UserRepositoryCommandLineRunner implements CommandLineRunner {
     public void run(String... args) throws Exception {
         User user = new User("Jill", "Admin");
         userRepository.save(user);
-        log.info("New User is created: " + user);
+        log.info("User is created: " + user);
+
+        Optional<User> userWithIdOne = userRepository.findById(1L);
+        log.info("User is retrieved: " + userWithIdOne);
+
+        List<User> users = userRepository.findAll();
+        log.info("All Users: " + users);
 
     }
 }
