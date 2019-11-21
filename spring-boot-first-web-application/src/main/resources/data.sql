978abc5a2cd83d026117c170ca9955ded2ec5e8d
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-11-21 14:13:07 +0700

    07 Connecting Web Application with JPA: 102 Step 31 Data initialization with data.sql

---
+++ spring-boot-first-web-application/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO TODO
+VALUES (10001, 'Learn Spring Boot', FALSE, sysdate(), 'gekif');
+INSERT INTO TODO
+VALUES (10002, 'Learn Angular JS', FALSE, sysdate(), 'gekif');
+INSERT INTO TODO
+VALUES (10003, 'Learn to Dance', FALSE, sysdate(), 'gekif');
